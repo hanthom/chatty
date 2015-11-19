a12808724b1fc5bae92376c0a59de4d6a476c372
hanthom <m.hantho@gmail.com>
2015-11-19 15:40:24 -0600

    Step one first commit

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+var http = require('http');
 
+var server = http.createServer();
+
+var messages = ["Hi", "Bye"];
+
+var handleRequest = function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	if (req.method === 'GET') {
+		res.statusCode = 200;
+
+
+
+		res.end(JSON.stringify({messages: messages});
+	}
+	
+}
+
+
+server
+	.on('request', handleRequest)
+	.listen(8887);
